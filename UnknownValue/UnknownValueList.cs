28f01a60861d4518af0eab03b37fb36ade224ae1
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2025-06-19 11:56:13 +0300

    immutability

--- UnknownValue/UnknownValueList.cs
+++ UnknownValue/UnknownValueList.cs
@@ -1,60 +1,63 @@
+using System.Collections.Immutable;
+
 public class UnknownValueList : UnknownTypedValue
 {
-    public HashSet<long> values = new();
+    readonly ImmutableHashSet<long> _values = ImmutableHashSet<long>.Empty;
 
     public UnknownValueList(TypeDB.IntInfo type, List<long> values = null) : base(type)
     {
         if (values != null)
-            this.values = new(values);
+            _values = ImmutableHashSet.CreateRange(values);
     }
 
     public UnknownValueList(TypeDB.IntInfo type, IEnumerable<long> values) : base(type)
     {
-        this.values = new(values);
+        if (values != null)
+            _values = ImmutableHashSet.CreateRange(values);
     }
 
     public override UnknownValueBase Add(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => MaskWithSign(v + l)))
+            ? new UnknownValueList(type, _values.Select(v => MaskWithSign(v + l)))
             : new UnknownValueList(type);
 
     public override UnknownValueBase Sub(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => MaskWithSign(v - l)))
+            ? new UnknownValueList(type, _values.Select(v => MaskWithSign(v - l)))
             : base.Sub(right);
 
     public override UnknownValueBase Div(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => v / l))
+            ? new UnknownValueList(type, _values.Select(v => v / l))
             : new UnknownValueList(type);
 
     public override UnknownValueBase Mod(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => v % l))
+            ? new UnknownValueList(type, _values.Select(v => v % l))
             : new UnknownValueList(type);
 
     public override UnknownValueBase Xor(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => v ^ l))
+            ? new UnknownValueList(type, _values.Select(v => v ^ l))
             : new UnknownValueList(type);
 
     public override UnknownValueBase ShiftLeft(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => MaskWithSign(v << (int)l)))
+            ? new UnknownValueList(type, _values.Select(v => MaskWithSign(v << (int)l)))
             : new UnknownValueList(type);
 
     public override UnknownValueBase SignedShiftRight(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => MaskWithSign(v >> (int)l)))
+            ? new UnknownValueList(type, _values.Select(v => MaskWithSign(v >> (int)l)))
             : new UnknownValueList(type);
 
     public override UnknownValueBase UnsignedShiftRight(object right) =>
         TryConvertToLong(right, out long l)
-            ? new UnknownValueList(type, values.Select(v => MaskWithSign(v >>> (int)l)))
+            ? new UnknownValueList(type, _values.Select(v => MaskWithSign(v >>> (int)l)))
             : new UnknownValueList(type);
 
-    public override UnknownValueBase Negate() => new UnknownValueList(type, values.Select(v => MaskWithSign(-v)));
-    public override UnknownValueBase BitwiseNot() => new UnknownValueList(type, values.Select(v => MaskWithSign(~v)));
+    public override UnknownValueBase Negate() => new UnknownValueList(type, _values.Select(v => MaskWithSign(-v)));
+    public override UnknownValueBase BitwiseNot() => new UnknownValueList(type, _values.Select(v => MaskWithSign(~v)));
 
     public override object Cast(TypeDB.IntInfo toType)
     {
@@ -71,30 +74,30 @@ public class UnknownValueList : UnknownTypedValue
 
     public override string ToString()
     {
-        if (values.Count <= 5)
-            return $"UnknownValueList<{type}>{{{string.Join(", ", values)}}}";
+        if (_values.Count <= 5)
+            return $"UnknownValueList<{type}>{{{string.Join(", ", _values)}}}";
         else
-            return $"UnknownValueList<{type}>[{values.Count}]";
+            return $"UnknownValueList<{type}>[{_values.Count}]";
     }
 
-    public override long Cardinality() => values.Count;
-    public override IEnumerable<long> Values() => values;
-    public override bool Contains(long value) => values.Contains(value);
-    public override long Min() => values.Min();
-    public override long Max() => values.Max();
+    public override long Cardinality() => _values.Count;
+    public override IEnumerable<long> Values() => _values;
+    public override bool Contains(long value) => _values.Contains(value);
+    public override long Min() => _values.Min();
+    public override long Max() => _values.Max();
 
     public override bool IntersectsWith(UnknownTypedValue other)
     {
-        return values.Any(v => other.Contains(v));
+        return _values.Any(v => other.Contains(v));
     }
 
-    public override bool Equals(object obj) => obj is UnknownValueList other && type == other.type && values.SequenceEqual(other.values);
+    public override bool Equals(object obj) => obj is UnknownValueList other && type == other.type && _values.SequenceEqual(other._values);
 
     public override int GetHashCode()
     {
         var hash = new HashCode();
         hash.Add(type);
-        foreach (var v in values)
+        foreach (var v in _values)
             hash.Add(v);
         return hash.ToHashCode();
     }
@@ -103,14 +106,14 @@ public class UnknownValueList : UnknownTypedValue
     {
         return other switch
         {
-            UnknownValueList l => new UnknownValueList(type, values.Union(l.values)),
-            byte b => Contains(b) ? this : new UnknownValueList(type, values.Concat(new[] { (long)b })),
-            sbyte sb => Contains(sb) ? this : new UnknownValueList(type, values.Concat(new[] { (long)sb })),
-            short s => Contains(s) ? this : new UnknownValueList(type, values.Concat(new[] { (long)s })),
-            ushort us => Contains(us) ? this : new UnknownValueList(type, values.Concat(new[] { (long)us })),
-            int i => Contains(i) ? this : new UnknownValueList(type, values.Concat(new[] { (long)i })),
-            uint ui => Contains(ui) ? this : new UnknownValueList(type, values.Concat(new[] { (long)ui })),
-            long l => Contains(l) ? this : new UnknownValueList(type, values.Concat(new[] { (long)l })),
+            UnknownValueList l => new UnknownValueList(type, _values.Union(l._values)),
+            byte b => Contains(b) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)b })),
+            sbyte sb => Contains(sb) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)sb })),
+            short s => Contains(s) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)s })),
+            ushort us => Contains(us) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)us })),
+            int i => Contains(i) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)i })),
+            uint ui => Contains(ui) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)ui })),
+            long l => Contains(l) ? this : new UnknownValueList(type, _values.Concat(new[] { (long)l })),
             // TODO: ulong
             _ => base.Merge(other)
         };
